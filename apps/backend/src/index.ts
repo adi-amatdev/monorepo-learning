95a57b07eb83b68802726c1eee0593f198d4d985
adi-amatdev <1rn20cs169.udupiaadityaacharya@gmail.com>
2025-07-31 11:30:50 +0530

    monorepo learning

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express'
+import { BACKEND_URL } from '@repo/types'
 
 const app = express();
 
@@ -7,4 +8,6 @@ app.get('/', (req: Request, res: Response) => {
     res.json({msg:"'Hello World"});
 });
 
-app.listen(3003);
+app.listen(3003,()=>{
+    console.log(`Server is running on ${BACKEND_URL}`);
+});
